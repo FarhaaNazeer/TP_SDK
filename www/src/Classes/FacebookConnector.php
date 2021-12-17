1d99c30a306775a36f4df0feaab8894848bf1887
FarhaaNazeer <farhaana18@gmail.com>
2021-12-17 15:12:48 +0100

    Ajout du state

--- www/src/Classes/FacebookConnector.php
+++ www/src/Classes/FacebookConnector.php
@@ -14,17 +14,19 @@ class FacebookConnector extends AuthConnectorFactory
     private $client_id;
     private $redirect_uri;
     private $scope;
+    private $state;
 
-    public function __construct(string $client_id, string $redirect_uri, string $scope)
+    public function __construct(string $client_id, string $redirect_uri, string $scope, string $state)
     {
 
         $this->client_id = $client_id;
         $this->redirect_uri = $redirect_uri;
         $this->scope = $scope;
+        $this->state= $state;
     }
 
     public function getAuthConnector(): AuthInterface
     {
-        return new FacebookAuth($this->client_id, $this->redirect_uri, $this->scope);
+        return new FacebookAuth($this->client_id, $this->redirect_uri, $this->scope, $this->state);
     }
 }
